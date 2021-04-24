13a41cc7594bccdaf571ea0dff8322a2323d44d4
Jose Luis Quintero <joseluisquisan@gmail.com>
2021-04-24 11:33:19 -0400

    Colocando el sistema de Vidas y el conteo de 10 preguntas

--- retomultiplicador.py
+++ retomultiplicador.py
@@ -3,22 +3,50 @@
 
 import random
 
-def desafio_multuplicador(nombre):
+intrucciones = '''
+================= Reto Multiplicador ================
+
+Tienes 3 Vidas y 10 Preguntas de Multiplicación que responder.
+
+Si llegas al final, viene un regalo Especial
+
+=====================================================
+
+'''
+
+def desafio_multuplicador(nombre, pregunta, vidas):
     valor_1= random.randint(1,5)
     valor_2= random.randint(1,5)
     resultado_verdadero = valor_1 * valor_2
-    resultado_elegido = int(input('Te pregunto, {}. ¿Cuanto es {} por {}? : '.format(nombre, valor_1, valor_2)))
+    resultado_elegido = int(input('Pregunta {}: Te pregunto, {}. ¿Cuanto es {} por {}? : '.format(pregunta, nombre, valor_1, valor_2)))
     if resultado_elegido == resultado_verdadero:
         print('Excelente! (Aplausos 👏👏👏)')
+        return vidas
     else:
+        vidas -= 1
         print('Lo siento, el valor verdadero es : {}'.format(resultado_verdadero))
+        print('Te quedad :', vidas)
+        return vidas
 
 def run():
     nombre = (input('Hola Hijo, ¿Cual es tu nombre?: ')).capitalize()
+    print('#' * 50)
     print('Bienvenido {} al Juego Multiplicador! (Aplausos 👏)'.format(nombre))
-    desafio_multuplicador(nombre)
-    desafio_multuplicador(nombre)
-    desafio_multuplicador(nombre)
-    
+    print('#' * 50)
+    print(intrucciones)
+    vidas = 3
+    pregunta = 1
+    while pregunta < 11:
+        if vidas > 0:
+            vidas = desafio_multuplicador(nombre, pregunta, vidas)
+            pregunta += 1
+        else:
+            print('#' * 50)
+            print('Oh, Lo siento, perdiste esta vez, vuelve a intentarlo en la próxima')
+            print('#' * 50)
+            break
+    else:
+        print('win!')
+
 if __name__ == '__main__':
     run()
